31dd69605b166f6c48483826b9323f06f775261c
kat-liu17 <zinanliu2019@u.northwestern.edu>
2025-11-04 11:53:26 -0500

    #KL Attempt minor update

--- KL attempt/Langchain_RWA Explain Trial KL.py
+++ KL attempt/Langchain_RWA Explain Trial KL.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()  
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
+print(OPENAI_API_KEY)
 
 import pandas as pd
 
